babc425ae4dad06ce9f3e78dc627c08d71aedd70
Johnssons <45001140+Johnssons@users.noreply.github.com>
2018-11-26 14:07:51 -0200

    Add files via upload

--- Q0122-Sketch/Q0122-Sketch.ino
+++ Q0122-Sketch/Q0122-Sketch.ino
@@ -1,37 +1,36 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 #define pinoRX 9
-#define pinoTX 10
+#define pinoTX 8
 
 SoftwareSerial serialGPS(pinoRX,pinoTX); // RX, TX
-TinyGPS gps1;
+TinyGPS gps;
 const String codPulseira = "1";
 float latitude, longitude;
 void setup() {
-  pinMode(pinoRX, INPUT);
-  pinMode(pinoTX, OUTPUT);
    serialGPS.begin(9600);
    Serial.begin(9600);
 }
 
 void loop() {
-  bool temInfo = false;
-  while (serialGPS.available())  
+  //Mostra os dados crus do GPS
+  bool newData = false;
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
   {
-    char data = serialGPS.read();
-    Serial.println(data);
-    if(gps1.encode(data))
-    temInfo = true;
+    while (serialGPS.available())
+    {
+      char c = serialGPS.read();
+      // Serial.write(c); //apague o comentario para mostrar os dados crus
+      if (gps.encode(c)) // Atribui true para newData caso novos dados sejam recebidos
+        newData = true;
+    }
   }
-  if (temInfo) 
+  if (newData)
   {
-     //Latitude e Longitude
-     gps1.f_get_position(&latitude, &longitude);
-        String sLat, sLon;
-        if(latitude != TinyGPS::GPS_INVALID_F_ANGLE && longitude != TinyGPS::GPS_INVALID_F_ANGLE)
-        {
-           Serial.println (codPulseira + "La" + latitude + "Lo" + longitude);
-        } 
+    float flat, flon;
+    String Sflat, Sflon;
+    gps.f_get_position(&flat, &flon);
+    Serial.print(codPulseira + "La" + (flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6) + "Lo" + (flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6));
   }
-  delay(5000);
 }
